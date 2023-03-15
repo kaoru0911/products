216fa604a1c56fb52c65ade4c34c96d5aabaaaaf
Ryder Tsai <kaoru0911@gmail.com>
2023-03-16 02:35:21 +0800

    add encoding

--- products.py
+++ products.py
@@ -15,6 +15,7 @@ for p in products:
 # 'abc' + '123' = 'abc123'
 # 'abc' * 3 = 'abcabcabc'
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品, 價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
